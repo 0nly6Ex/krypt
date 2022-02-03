d07311ea156451380ea454583ac72a460fceac62
0nly6Ex <76264172+0nly6Ex@users.noreply.github.com>
2022-02-03 09:03:05 -0500

    Update crack.lua

--- Serenity/crack.lua
+++ Serenity/crack.lua
@@ -1,37 +1,37 @@
--- 6Ex still on top LMFAO
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/0nly6Ex/krypt/main/Serenity/resources/serenity.lua"))() -- the serenity > all thing is mad cap LMAO
-wait(2.9) -- thats how long their shitty script takes to load :troll:
-local old = game.HttpGetAsync
-hookfunc(game.HttpGet, function(e, i)
-    if i == "https://uhh98g4h89s.000webhostapp.com/OgueyG73f.php" then
-        return "6ExOnTop"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0nly6Ex/krypt/main/Serenity/resources/serenity.lua"))()
+wait(2.9)
+local a = game.HttpGetAsync
+hookfunc(
+    game.HttpGet,
+    function(b, c)
+        if c == "https://uhh98g4h89s.000webhostapp.com/OgueyG73f.php" then
+            return "6ExOnTop"
+        end
+        return a(b, c)
     end
-    return old(e, i)
-end)
--- the crack part (decided to keep it open source so you can see how easy it is to crack)
-local targetgui = nil
-local function getstringlength(string)
-    return string.len(string)
+)
+local d = nil
+local function e(f)
+    return f.len(f)
 end
-for _, v in pairs(game.CoreGui:GetChildren()) do
-    if v.Name then
-        if getstringlength(v.Name) > 20 then
-            targetgui = v
-            setclipboard(targetgui.Name)
+for g, h in pairs(game.CoreGui:GetChildren()) do
+    if h.Name then
+        if e(h.Name) > 20 then
+            d = h
+            setclipboard(d.Name)
         end
     end
 end
-if targetgui then
-targetgui.Shadow.Main.KeyFrame.Key.Text = "Cracked by 6Ex"
-targetgui.Shadow.Main.GetKey.Text = "6Ex#6666"
-wait(3)
-for i = 1, 5 do
-    targetgui.Shadow.Main.GetKey.Text = "6Ex#6666"
-targetgui.Shadow.Main.GetKey.Text = "6Ex#6666"
-targetgui.Shadow.Main.Submit.Text = "6Ex#6666"
-targetgui.Shadow.Main.KeyFrame.Key.Text = "6ExOnTop"
-connection = targetgui.Shadow.Main.Submit.MouseButton1Click
-firesignal(connection)
-end
+if d then
+    d.Shadow.Main.KeyFrame.Key.Text = "Cracked by 6Ex"
+    d.Shadow.Main.GetKey.Text = "6Ex#6666"
+    wait(3)
+    for c = 1, 5 do
+        d.Shadow.Main.GetKey.Text = "6Ex#6666"
+        d.Shadow.Main.GetKey.Text = "6Ex#6666"
+        d.Shadow.Main.Submit.Text = "6Ex#6666"
+        d.Shadow.Main.KeyFrame.Key.Text = "6ExOnTop"
+        connection = d.Shadow.Main.Submit.MouseButton1Click
+        firesignal(connection)
+    end
 end
